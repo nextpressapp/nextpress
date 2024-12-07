e2539cc1927d88462712a46f3fc6067b02513709
dragon <allanice001@gmail.com>
2024-12-07 01:59:40 +0000

    debugging verify-email in vercel

--- src/app/api/auth/verify-email/route.ts
+++ src/app/api/auth/verify-email/route.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/prisma";
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const token = searchParams.get("token");
+  console.log('Token:', token);
 
   if (!token) {
     return NextResponse.json({ error: "Missing token" }, { status: 400 });
@@ -14,6 +15,7 @@ export async function GET(req: Request) {
       where: { token },
     });
 
+    console.log(verificationToken);
     if (!verificationToken) {
       return NextResponse.json({ error: "Invalid token" }, { status: 400 });
     }
@@ -30,6 +32,7 @@ export async function GET(req: Request) {
 
     await prisma.verificationToken.delete({ where: { token } });
 
+    console.log('I GET HERE')
     return NextResponse.redirect(
       process.env.NEXT_PUBLIC_APP_URL + "/auth/login",
       { status: 307 },
